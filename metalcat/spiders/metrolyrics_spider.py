47105d4b47e596e36d9616fb319e1f46a2e44514
Allie Crevier <arcrevier@gmail.com>
2017-05-22 20:45:28 -0400

    Update spider to use input params for song and artist

--- metalcat/spiders/metrolyrics_spider.py
+++ metalcat/spiders/metrolyrics_spider.py
@@ -1,18 +1,24 @@
 from scrapy.spiders import Spider
 from scrapy.selector import HtmlXPathSelector
+from scrapy import Request
+
 from metalcat.items import MetrolyricsItem
 
 class MetrolyricsSpider(Spider):
     name = "metrolyrics_spider"
     allowed_domains = ["metrolyrics.com"]
-    start_urls = ["http://www.metrolyrics.com/dopesmoker-lyrics-sleep.html"]
+
+    def start_requests(self):
+        song = getattr(self, 'song', 'cat-scratch-fever')
+        artist = getattr(self, 'artist', 'ted-nugent')
+        url = 'http://www.metrolyrics.com/'+song+'-lyrics-'+artist+'.html'
+        yield Request(url, self.parse)
 
     def parse(self, response):
-        hxs = HtmlXPathSelector(response)
-        verses = hxs.select('//div[@id="lyrics-body-text"]/p[@class="verse"]')
+        verses = response.xpath('//div[@id="lyrics-body-text"]/p[@class="verse"]')
         items = []
         for verse in verses:
             item = MetrolyricsItem()
-            item['lines'] = verse.select('text()').extract()
+            item['lines'] = verse.xpath('text()').extract()
             items.append(item)
         return items
