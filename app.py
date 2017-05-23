6ea7e07dfc07b90a70e1299bf78b8e1387ffd6c4
Allie Crevier <arcrevier@gmail.com>
2017-05-22 20:27:44 -0400

    Add method to get the right sized overlay text

--- app.py
+++ app.py
@@ -15,21 +15,30 @@ app.config.update(DEBUG = True)
 
 @app.route("/")
 def main():
-    overlayLyricsOnCatPhoto()
+    overlayLyricsOnCatImage()
     return render_template('index.html')
 
-def overlayLyricsOnCatPhoto():
-    with open('output/lyrics.json') as json_data:
-        verses = json.load(json_data)
-        first_line = verses[0]['lines'][0]
-        second_line = verses[0]['lines'][1]
-
-    img = Image.open("static/cat.png")
+def overlayLyricsOnCatImage():
+    overlay_text = getOverlayText();
+    img = Image.open('static/cat.png')
     draw = ImageDraw.Draw(img)
     font = ImageFont.truetype('static/Impact.ttf', 60)
-    draw.text((25, 50), first_line, (200,40,40), font=font)
-    draw.text((75, 100), second_line, (200,40,40), font=font)
+    draw.text((50, 50), overlay_text[0], (200,40,40), font=font)
+    draw.text((80, 100), overlay_text[1], (200,40,40), font=font)
     img.save('output/metalcat.jpg')
+
+def getOverlayText():
+    overlay_text = ['=^..^=', '']
+    with open('output/lyrics.json') as json_data:
+        verses = json.load(json_data)
+        for verse in verses:
+            lines = verse['lines']
+            if len(lines) >= 2:
+                if (len(lines[0]) < 47 and len(lines[1]) < 47):
+                    overlay_text[0] = lines[0]
+                    overlay_text[1] = lines[1]
+                    break
+    return overlay_text
 
 if __name__ == "__main__":
     app.run()
